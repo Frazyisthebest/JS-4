51d60a7f7a0e64e6814c44f7ec43f689c09ef0cb
khabibullo <khabibullo@gmail.com>
2021-09-05 18:20:21 +0500

    JS-4

---
+++ script.js
@@ -0,0 +1,30 @@
+var name = prompt('Введите своё имя')
+    born = prompt('Введи свой год рождения')
+    year = prompt('Введите настоящее время ')
+
+function myself() {
+    let myname = name
+    let age =', Ваш возраст ' + (year , born)
+    alert(myname + age)
+}
+myself()
+
+let number = prompt('Введите число')
+
+for (let i = 1;i <= number;i++) { 
+   function first(min,max) {
+       let num = Math.floor(Math.random() * (max + 1 - min) + min)
+       return num;
+   }
+   function second(min,max) {
+       let num2 = Math.floor(Math.random() * (max + 1 - min) + min)
+       return num2;
+   }
+   
+   let ask = prompt(first + second)
+}
+
+
+
+
+    
